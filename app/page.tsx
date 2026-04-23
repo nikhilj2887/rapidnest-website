9695fc5c8f7f7689c4aa0001415002995ffbd5df
nikhilj2887 <jjnikhil@gmail.com>
2026-04-23 12:41:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import WhatsAppButton from "./components/WhatsAppButton";
+import WhyChooseUs from "./components/WhyChooseUs";
+import Contact from "./components/Contact";
+import Clients from "./components/Clients";
+
 export default function Home() {
   return (
-    <div>
-      Website working test
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Clients />
+      <WhyChooseUs />
+      <About />
+      <Contact />
+      <Footer />
+      <WhatsAppButton />
+    </main>
   );
 }
