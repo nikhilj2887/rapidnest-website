e558fbbc87351dbac457d2db9e3dd2a4a648c240
nikhilj2887 <jjnikhil@gmail.com>
2026-04-23 11:35:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,7 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Services from "./components/Services";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import WhatsAppButton from "./components/WhatsAppButton";
-import WhyChooseUs from "./components/WhyChooseUs";
-import Contact from "./components/Contact";
-import Clients from "./components/Clients";
-
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Services />
-      <Clients />
-      <WhyChooseUs />
-      <About />
-      <Contact />
-      <Footer />
-      <WhatsAppButton />
-    </main>
+    <div>
+      Website working test
+    </div>
   );
 }
